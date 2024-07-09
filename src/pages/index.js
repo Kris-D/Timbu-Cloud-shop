24b036943d8b837460c8513b916f18e940fbcf25
Christopher Isioma Dunkwu <chriscjd001@gmail.com>
2024-07-09 12:34:49 +0100

    Route and pages set up

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Productlist } from "./productlist/Productlist";
+export { default as Cart } from "./cart/Cart";
+export { default as AccountCreation } from "./accountCreation/AccountCreation";
+export { default as PaymentMethod } from "./paymentMethod/PaymentMethod";
+export { default as ConfirmOrder } from "./confirmOrder/ConfirmOrder";
+export { default as SuccessfullyOrder } from "./successfullyOrder/SuccessfullyOrder";
---
+++ src/pages/successfullyOrder/SuccessfullyOrder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SuccessfullyOrder = () => {
+  return (
+    <div>SuccessfullyOrder</div>
+  )
+}
+
+export default SuccessfullyOrder
