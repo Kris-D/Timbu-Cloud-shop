d01a38b3ee12e546893674292c53afa45a4da86c
Christopher Isioma Dunkwu <chriscjd001@gmail.com>
2024-07-09 19:57:08 +0100

    productlist progress

---
+++ src/components/tabledata.js
@@ -0,0 +1,44 @@
+export const productlist = [
+    {
+        id: "1",
+        img:"./images/1.svg",
+        name:"Mac Lipstick",
+        price: "N5000",
+        star: 5,
+    },
+    {
+        id: "2",
+        img: "./images/2.svg",
+        name:"Mac Lipstick",
+        price: "N5000",
+        star: 3,
+    },
+    {
+        id: "3",
+        img: "./images/3.svg",
+        name:"Mac Lipstick",
+        price: "N5000",
+        star: 4,
+    },
+    {
+        id: "4",
+        img: "./images/4.svg",
+        name:"Mac Lipstick",
+        price: "N5000",
+        star: 4,
+    },
+    {
+        id: "5",
+        img: "./images/5.svg",
+        name:"Mac Lipstick",
+        price: "N5000",
+        star: 4,
+    },
+    {
+        id: "6",
+        img: "./images/6.svg",
+        name:"Mac Lipstick",
+        price: "N5000",
+        star: 4,
+    },
+]
--- src/pages/productlist/Productlist.jsx
+++ src/pages/productlist/Productlist.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
-
+import { CiSearch, CiShoppingCart, CiUser } from 'react-icons/ci'
+import styles from "./Productlist.module.css";
+import logo from "../../assets/logo.svg";
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
+import {productlist } from '../../components/tabledata';
+import { IoMdStarOutline } from 'react-icons/io';
 const Productlist = () => {
   return (
-    <div>Productlist</div>
+    <div className={styles.wrap}>
+     <div className={styles.head}>
+      <div className={styles.logocart}>
+       <img src={logo} alt="shop-logo" />
+       <div className= {styles.icons}>
+       <CiSearch />
+       <CiUser />
+       <CiShoppingCart/>
+       </div>
+      </div>
+     </div>
+     <div className={styles.filterbyproduct}>
+       <div className={styles.filterbyproducthead}>
+         <p>Filter by</p>
+         <div className={styles.headboxes}>
+         <div className={styles.filterbox}>
+                <span>Price</span>
+                <MdOutlineKeyboardArrowDown />
+              </div>
+         <div className={styles.filterbox}>
+                <span>Rating</span>
+                <MdOutlineKeyboardArrowDown />
+              </div>
+         <div className={styles.filterbox3}>
+                <span>Best Sellers</span>
+                <MdOutlineKeyboardArrowDown />
+              </div>
+         </div>
+       </div>
+       <div className={styles.productlist}>
+       {productlist.map(product => (
+        <div key={product.id} className={styles.productitem}>
+          <img src={product.img} alt={product.name} width={240} height={200} />
+          <div className="">
+          <p>{product.name}</p>
+          <p>{product.price}</p>
+          <p>{Array.from({ length: product.star}).map((_, index) => (
+              <IoMdStarOutline key={index} />
+            ))}</p>
+          </div>
+        </div>
+      ))}
+       </div>
+
+     </div>
+
+    </div>
   )
 }
 
